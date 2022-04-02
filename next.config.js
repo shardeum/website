611e32ff22ce88337f1f4e97bca574e3b19728ba
apurvc99 <apurv@venwiz.com.com>
2022-04-02 16:58:29 +0530

    added: ✅  next-i18next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
+import i18n from "./next-i18next.config";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  i18n,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

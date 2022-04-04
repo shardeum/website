92a192ac92b669e3357731b865f683f84068829b
Anurag Bhagsain <abhagsain@gmail.com>
2022-04-04 15:25:22 +0530

    refactor: 🛠 add fonts and theme colors

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import customTheme from "../theme";
+import "../styles/satoshi.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (

2df5685d0d2d95a7010baac2d63dac14f374b4d3
Anurag Bhagsain <abhagsain@gmail.com>
2022-04-04 17:17:40 +0530

    feat: ✅ add Join Community Section

---
+++ layout/sections/JoinCommunity.tsx
@@ -0,0 +1,96 @@
+import {
+  Box,
+  Container,
+  Flex,
+  HStack,
+  SimpleGrid,
+  Text,
+  useColorMode,
+  VStack,
+} from "@chakra-ui/react";
+import Link from "next/link";
+import {
+  Discord,
+  ExternalIcon,
+  Github,
+  SeeMore,
+  Telegram,
+  Twitter,
+} from "./Icons";
+
+const socialLinks = [
+  { Icon: Discord, title: "Discord", href: "https://discord.gg/yWqQZQ" },
+  { Icon: Twitter, title: "Twitter", href: "https://discord.gg/yWqQZQ" },
+  { Icon: Github, title: "Github", href: "https://discord.gg/yWqQZQ" },
+  { Icon: Telegram, title: "Telegram", href: "https://discord.gg/yWqQZQ" },
+  { Icon: SeeMore, title: "See more", href: "https://discord.gg/yWqQZQ" },
+];
+
+const JoinCommunity = () => {
+  return (
+    <Flex bg="brand.grey-90" as="section">
+      <Container maxW="container.xl" mx="auto" pt="16" pb="28">
+        <SimpleGrid columns={[1, 1, 2]} gap={["8", "12"]}>
+          <VStack spacing="6">
+            <Text
+              as="h2"
+              textAlign={{ base: "center", md: "left" }}
+              lineHeight="normal"
+              fontSize={{ base: "4xl", md: "5xl", lg: "7xl" }}
+              fontWeight="bold"
+              color="brand.white"
+            >
+              Join the Community
+            </Text>
+            <Box maxW={{ base: "md", md: "full" }}>
+              <Text
+                fontSize={{ base: "md", lg: "base" }}
+                textAlign={{ base: "center", md: "left" }}
+                color="brand.grey-40"
+              >
+                Shardeum is a global community. Anyone can join us in the
+                journey to onboard billions of people into Web 3. Don't know how
+                to code? Or not sure where to start? Don't worry. You can start
+                by contributing as a content creator, community manager,
+                language translator, developer and you name it
+              </Text>
+            </Box>
+          </VStack>
+          <Flex justifyContent="flex-end">
+            <VStack w="96">
+              {socialLinks.map((link) => (
+                <Link href="#" passHref>
+                  <HStack
+                    as="a"
+                    w="full"
+                    key={link.title}
+                    py="5"
+                    borderBottom="1px"
+                    borderColor="brand.grey-80"
+                    justifyContent="space-between"
+                  >
+                    <HStack spacing="4">
+                      <link.Icon />
+                      <Text
+                        color="brand.white"
+                        fontSize="base"
+                        fontWeight="normal"
+                        w="full"
+                        _hover={{ color: "brand.grey-40" }}
+                      >
+                        {link.title}
+                      </Text>
+                    </HStack>
+                    <ExternalIcon />
+                  </HStack>
+                </Link>
+              ))}
+            </VStack>
+          </Flex>
+        </SimpleGrid>
+      </Container>
+    </Flex>
+  );
+};
+
+export default JoinCommunity;

2cf69eae1d74d81125943a97e56e2b33e111ae3a
Anurag Bhagsain <abhagsain@gmail.com>
2022-04-25 01:22:57 +0530

    refactor: 🛠 add eslint rules and airtable to img src

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,17 @@
 import { Box } from "@chakra-ui/react";
 import type { NextPage } from "next";
+import "swiper/css";
+import "swiper/css/pagination";
+import SlidingStats from "../components/common/SlidingStats";
 import Navbar from "../components/Navbar";
 import Footer from "../layout/sections/Footer";
 import Hero from "../layout/sections/Hero";
 import JoinCommunity from "../layout/sections/JoinCommunity";
+import MoreAboutShardeum from "../layout/sections/MoreAboutShardeum";
+import ReadWhitepaper from "../layout/sections/ReadWhitepaper";
 import Roadmap from "../layout/sections/Roadmap";
+import ShardeumInNews from "../layout/sections/ShardeumInNews";
+import SHMTokenomics from "../layout/sections/SHMTokenomics";
 import Team from "../layout/sections/Team";
 import UseCases from "../layout/sections/UseCases";
 
@@ -13,9 +20,14 @@ const Home: NextPage = () => {
     <Box as="main">
       <Navbar />
       <Hero />
+      <SlidingStats />
+      <ReadWhitepaper />
+      <MoreAboutShardeum />
+      <SHMTokenomics />
       <UseCases />
       <Roadmap />
       <Team />
+      <ShardeumInNews />
       <JoinCommunity />
       <Footer />
     </Box>

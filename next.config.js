da28840dcff7a22da32dff845f8dcb6d855c7ca0
anvaynk <anvaynk@gmail.com>
2022-05-03 20:02:38 +0530

    dev: Implemented proxy for blog.

--- next.config.js
+++ next.config.js
@@ -1,8 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  trailingSlash: true,
   images: {
     domains: ["airtable.com", "dl.airtable.com"],
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/blog/:slug*",
+        destination: `${process.env.BLOG_URL}/:slug*`,
+      },
+    ];
+  },
+  async headers() {
+    return [
+      {
+        source: "/blog/:slug*",
+        headers: [
+          { key: "x-forwarded-proto", value: "https" },
+          { key: "x-forwarded-host", value: process.env.ENV_DOMAIN },
+          { key: "host", value: process.env.ENV_DOMAIN },
+        ],
+      },
+    ];
   },
 };
 

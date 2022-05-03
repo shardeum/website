76d710cd6d58fd6d3a5db3e2a819b89cd264fd6b
Dewansh <dewanshparashar@gmail.com>
2022-05-03 20:53:56 +0530

    Dev : Added favicons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,10 +8,20 @@ import { Box } from "@chakra-ui/react";
 import Navbar from "components/sections/Navbar";
 import Footer from "components/sections/Footer";
 import defaultSEOValues from "../next-seo.config";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={customTheme}>
+      <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png" />
+        <link rel="manifest" href="favicon/site.webmanifest" />
+        <link rel="mask-icon" href="favicon/safari-pinned-tab.svg" color="#000000" />
+        <meta name="msapplication-TileColor" content="#000000" />
+        <meta name="theme-color" content="#000000"></meta>
+      </Head>
       <Box as="main">
         {/* common header  */}
         <Navbar />

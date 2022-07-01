cc1feb8b458de344d16559081a1e82eb210f846a
snymrova <luthra.sunny@gmail.com>
2022-07-01 11:33:57 +0530

    greenhouse updates

--- pages/careers/index.tsx
+++ pages/careers/index.tsx
@@ -1,56 +1,47 @@
-import { Box, Button, Stack } from "@chakra-ui/react";
+import { Box, Flex, Container } from "@chakra-ui/react";
 import type { NextPage } from "next";
 import Hero from "components/sections/Hero";
-import JoinCommunity from "components/sections/JoinCommunity";
-import Roadmap from "../../components/sections/alphanet/Roadmap";
-import UseCases from "components/sections/UseCases";
-import FAQs from "components/sections/FAQs";
 import Image from "next/image";
-import { AlphanetFeatureIcons } from "@shm/Icons";
 import { NextSeo } from "next-seo";
-import { CLAIM_100_SHM_LINK, DOCS_URL } from "constants/links";
 import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-import WhatCanYoDo from "@shm/components/sections/WhatCanYouDo";
 import Script from "next/script";
+import { useRouter } from "next/router";
 
 const Carrers: NextPage = () => {
+  const router = useRouter();
+
   const { t: pageTranslation } = useTranslation(["page-careers"]);
   const { t: commonTranslation } = useTranslation(["common"]);
   return (
     <>
       <NextSeo
-        title={
-          "Shardeum Liberty | Alphanet | Build scalable dApps and Web 3.0 applications on Shardeum"
-        }
+        title={"Shardeum Careers | Help us build Shardeum!"}
         description="Shardeum is the world’s first layer 1 blockchain that truly solves scalability trilemma. It is an EVM based smart contract network that scales linearly with low gas fees forever with an aim to onboard billions of daily users and numerous DApps to Web 3"
-        canonical="https://shardeum.org/shardeum-liberty-alphanet/"
+        canonical="https://shardeum.org/careers/"
         additionalMetaTags={[
           {
             property: "keywords",
-            content:
-              "shardeum, shardeum liberty, testnet, alphanet, blockchain,layer1 blockchain,evm compatible blockchain",
+            content: "shardeum,blockchain,layer1 blockchain,evm compatible blockchain",
           },
           {
             property: "twitter:image",
-            content: "https://shardeum.org/shardeum-liberty.jpeg",
+            content: "https://shardeum.org/Shardeum.png",
           },
         ]}
         openGraph={{
-          url: "https://shardeum.org/shardeum-liberty-alphanet/",
-          title: "Shardeum Liberty | Alphanet | Build your DApps and Web3 services on Shardeum",
+          title: "Shardeum Careers | Help us build Shardeum!",
+          type: "website",
+          url: "https://shardeum.org/careers/",
+
           description:
-            "Shardeum is the world’s first layer 1 blockchain that truly solves scalability trilemma. It is an EVM based smart contract network that scales linearly with low gas fees forever with an aim to onboard billions of daily users and numerous DApps to Web 3",
+            "Shardeum is the world's first EVM based L1 blockchain that truly solves scalability trilemma with linear scaling and low gas fees forever",
           images: [
             {
-              url: "https://shardeum.org/shardeum-liberty.jpeg",
-              width: 800,
-              height: 600,
-              alt: "Shardeum Liberty | Alphanet | Build your DApps and Web3 services on Shardeum",
-              type: "image/jpeg",
+              url: "https://shardeum.org/Shardeum.png",
+              alt: "Shardeum Image",
             },
           ],
-          site_name: "Shardeum Liberty | Alphanet | Build your DApps and Web3 services on Shardeum",
         }}
         twitter={{
           cardType: "summary_large_image",
@@ -60,23 +51,33 @@ const Carrers: NextPage = () => {
       />
 
       {/* Hero section */}
-      <Hero
-        heading="Help us build Shardeum!"
-        description="Join the team of enthusiastic people who are striving to solve blockchain's biggest problems"
-        media={
-          <Box position="relative" h="full">
-            <Image
-              objectFit="contain"
-              src="/careers/careers-hero.png"
-              alt="Shardeum Alphanet is Live"
-              layout="fill"
-            />
-          </Box>
-        }
-      />
-
-      <div id="grnhse_app"></div>
-      <Script src="https://boards.greenhouse.io/embed/job_board/js?for=shardeumfoundation" />
+      {!router.query.gh_jid && (
+        <Hero
+          heading="Help us build Shardeum!"
+          description="Join the team of enthusiastic people who are striving to solve blockchain's biggest problems"
+          media={
+            <Box position="relative" h="full">
+              <Image
+                objectFit="contain"
+                src="/careers/careers-hero.png"
+                alt="Shardeum Alphanet is Live"
+                layout="fill"
+              />
+            </Box>
+          }
+        />
+      )}
+      <Flex bg="brand.white" as="section">
+        <Container
+          maxW="container.xl"
+          mx="auto"
+          py={{ base: "9", md: "20" }}
+          px={{ base: 6, xl: 0 }}
+        >
+          <div id="grnhse_app"></div>
+          <Script src="https://boards.greenhouse.io/embed/job_board/js?for=shardeumfoundation" />
+        </Container>
+      </Flex>
     </>
   );
 };
@@ -84,7 +85,7 @@ const Carrers: NextPage = () => {
 export async function getStaticProps({ locale }: { locale: string }) {
   return {
     props: {
-      ...(await serverSideTranslations(locale, ["common", "page-alphanet"])),
+      ...(await serverSideTranslations(locale, ["common", "page-careers"])),
       // Will be passed to the page component as props
     },
   };

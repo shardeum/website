0f575029125b269abb8d957a88af7e2099cd84d9
lanxeon <bswapnonil@yahoo.com>
2022-07-23 21:56:40 +0530

    added async utility helper

---
+++ pages/api/project/upvote.ts
@@ -0,0 +1,15 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { getSession } from "next-auth/react";
+import { gosync } from "utils/async-helpers";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const [session, err] = await gosync(getSession());
+
+  if (err || !session) {
+    return res.status(401).json({ message: "Unauthorized access" });
+  }
+
+  console.log(session);
+
+  return res.status(200).json({ session, err });
+}

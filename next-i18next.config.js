e8d41770e0d940bbcbf8f3b3e115e44e310ab5db
snymrova <luthra.sunny@gmail.com>
2022-09-28 15:19:59 +0530

    eco push

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,9 @@
+import path from "path";
+
 module.exports = {
   i18n: {
     defaultLocale: "en",
     locales: ["en", "hi", "id", "it", "ja", "kn", "ko", "ru", "ta", "te"],
+    localePath: path.resolve("./public/locales"),
   },
 };

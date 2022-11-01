486d2bb607507fabf5fe4b53e84ee941aae2b3b1
Abhinav Roy <danroy48@gmail.com>
2022-11-01 09:04:25 +0530

    change

--- constants/links.ts
+++ constants/links.ts
@@ -26,4 +26,4 @@ export const GENERAL_QUERIES_LINK = "mailto:hi@shardeum.org";
 export const INVESTMENT_QUERY_LINK = "mailto:invest@shardeum.org";
 export const CROWDIN_PROJECT_LINK = "https://crowdin.com/project/shardeumtranslation";
 export const CLAIM_100_SHM_LINK = "https://docs.shardeum.org/faucet/claim";
-export const EXPLORER_LIBERTY_URL = "https://explorer.liberty10.shardeum.org/";
+export const EXPLORER_LIBERTY_URL = "https://explorer-liberty10.shardeum.org/";

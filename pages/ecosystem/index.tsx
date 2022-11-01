b4c154196ac3e9712b4650a825a5c3f1cb28dbea
Abhinav Roy <danroy48@gmail.com>
2022-11-01 08:45:28 +0530

    change

--- pages/ecosystem/index.tsx
+++ pages/ecosystem/index.tsx
@@ -9,7 +9,7 @@ import TrendingProjects from "@shm/components/sections/explore/TrendingProjects"
 import NewestProjects from "@shm/components/sections/explore/NewProjects";
 import { useTranslation } from "next-i18next";
 
-import { getSession } from "next-auth/react";
+import { getSession, useSession } from "next-auth/react";
 import { getSHMProjects, getUserUpvotedProjects } from "utils/api";
 import { upvoteProject } from "services/explore.service";
 import SigninContext from "context/signin-window.context";
@@ -43,6 +43,8 @@ const Explore: NextPage<ExplorePageProps> = ({
 }: ExplorePageProps) => {
   // to open signin window
   const { setPopup } = useContext(SigninContext);
+  const { data: sessionData } = useSession();
+  console.log(sessionData);
 
   // convert server props into state
   const [projectsState, setProjectsState] = useState(projects);

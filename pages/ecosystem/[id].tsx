f92c430c26a84be4a6783326451f4ba84e5d2e1f
Ritesh Rana <rana1997ritesh@gmail.com>
2023-01-31 13:19:00 +0530

    fix ecosystem

--- pages/ecosystem/[id].tsx
+++ pages/ecosystem/[id].tsx
@@ -9,6 +9,7 @@ import { getSession } from "next-auth/react";
 import ShareModal from "@shm/components/sections/explore/Details/ShareModal";
 import { ProductScreenshots } from "@shm/components/sections/explore/Details/ProductScreenshots";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { Helmet } from "react-helmet";
 
 export const getServerSideProps = async ({ req, locale, query }: GetServerSidePropsContext) => {
   const session = await getSession({ req });
@@ -37,18 +38,64 @@ export const ExploreDetails: NextPage<ProjectPageProps> = ({
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   useEffect(() => {
-    const prevTitle = document.title;
-    document.title = `Shardeum | Ecosystem | ${project.name}`;
-    const Description = project.description.substring(0, 160);
-    const stateObj = {};
-    return () => {
-      document.title = prevTitle;
-      const CONTENT = document.getElementsByTagName("META") as any | null;
-      CONTENT[5].content = Description;
-    };
+    // const stateObj = {};
+    // const prevTitle = document.title;
+    // const Description = project.description.substring(0, 160);
+    // const CONTENT = document.getElementsByTagName("META") as any | null;
+    // CONTENT[5].content = Description;
+    const pageTitle = `Shardeum | Ecosystem | ${project.name}`;
+    if (document.title !== pageTitle) {
+      document.title = pageTitle;
+    }
   });
   return (
     <Box>
+      <Helmet>
+        <title>{`Shardeum | Ecosystem ${project.name}`}</title>
+        <meta name="description" content={project.description.substring(0, 160)} />
+        <meta
+          name="keywords"
+          content="shardeum,blockchain,layer1 blockchain,evm based blockchain"
+        />
+
+        {/* Facebook */}
+        <meta
+          property="og:url"
+          content={`https://shardeum.org/ecosystem/${project.name.replace(/\s/g, "")}`}
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={`Shardeum | Ecosystem ${project.name}`} />
+        <meta property="og:description" content={`${project.description.substring(0, 160)}`} />
+        <meta
+          property="og:image"
+          content={project.logo || `https://shardeum.org/wp-content/uploads/2022/03/Shardeum.png`}
+        />
+
+        {/* Twiter */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="https://shardeum.org/" />
+        <meta
+          property="twitter:url"
+          content={`https://shardeum.org/ecosystem/${project.name.replace(/\s/g, "")}`}
+        />
+        <meta property="twitter:title" content={`Shardeum | Ecosystem ${project.name}`} />
+        <meta property="twitter:description" content={`${project.description.substring(0, 160)}`} />
+        <meta
+          property="twitter:image"
+          content={project.logo || `https://shardeum.org/wp-content/uploads/2022/03/Shardeum.png`}
+        />
+
+        {/* <meta property="og:site_name" content={`Shardeum | Ecosystem ${project.name}`} /> */}
+
+        {/* <meta name="twitter:title" content={`Shardeum | Ecosystem ${project.name}`} /> */}
+        {/* <meta name="twitter:description" content={project.description.substring(0, 160)} /> */}
+        {/* <meta
+          name="twitter:image"
+          content={project.logo || `https://shardeum.org/wp-content/uploads/2022/03/Shardeum.png`}
+        /> */}
+        <meta name="twitter:site" content="@shardeum" />
+        <link rel="canonical" href="https://shardeum.org/" />
+      </Helmet>
       <HorizontalTile
         onOpen={onOpen}
         project={project}

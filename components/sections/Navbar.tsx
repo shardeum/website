4c7da6d3fb6103e1b7b2989a60d749bbe9069613
Abhinav Roy <danroy48@gmail.com>
2023-04-13 14:49:53 +0530

    update

--- components/sections/Navbar.tsx
+++ components/sections/Navbar.tsx
@@ -267,7 +267,7 @@ const Navbar: FC<NavbarProps> = ({ mode = "dark" }) => {
         // className={!scrollHideNoti ? "navNotificationHide" : "navNotificationShow"}
         // className="navNotificationShow"
       >
-        <NotificationBar />
+        {/* <NotificationBar /> */}
         {/* {hideNoti === false ? <NotificationBar /> : null} */}
         <Flex
           bg={mode === "light" ? "brand.white" : "brand.black"}

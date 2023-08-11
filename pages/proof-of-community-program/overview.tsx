316370dd27059e3f2af0ace1fa656f93cc8192fd
sunny luthra <luthra.sunny@gmail.com>
2023-08-11 11:15:01 +0530

    overview

---
+++ pages/proof-of-community-program/overview.tsx
@@ -0,0 +1,136 @@
+/* eslint-disable react/no-unknown-property */
+import { NotionAPI } from "notion-client";
+import { Container, Flex, Text, VStack } from "@chakra-ui/react";
+import { ExtendedRecordMap } from "notion-types";
+import { NotionRenderer } from "react-notion-x";
+// core styles shared by all of react-notion-x (required)
+import "react-notion-x/src/styles.css";
+import NotionPagesLinks from "constants/notion";
+import { NextSeo } from "next-seo";
+import { getPageTitle } from "notion-utils";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useTranslation } from "next-i18next";
+import NextLink from "next/link";
+import Hero from "components/sections/Hero";
+import TweetEmbed from "react-tweet-embed";
+import { Collection } from "react-notion-x/build/third-party/collection";
+
+const Page = ({
+  recordMap,
+  notionPageDetails,
+}: {
+  recordMap: ExtendedRecordMap;
+  notionPageDetails: any;
+}) => {
+  const Tweet = ({ id }: { id: string }) => {
+    return <TweetEmbed tweetId={id} />;
+  };
+  const title = notionPageDetails.title || getPageTitle(recordMap);
+  const canonical = "https://shardeum.org/" + notionPageDetails.slug + "/";
+  const description = notionPageDetails.description;
+  const image = notionPageDetails.image;
+  return (
+    <>
+      <Hero
+        cta={
+          <>
+            <Text
+              fontSize={{ base: "md", lg: "xl" }}
+              textAlign="left"
+              lineHeight={{ base: "7", md: "8" }}
+              // color={"#37352f"}
+              // paddingLeft="7.9cm"
+            >
+              <p>
+                <NextLink href="/" passHref>
+                  Home
+                </NextLink>{" "}
+                / Proof Of Community
+              </p>
+            </Text>
+          </>
+        }
+      />
+      <NextSeo
+        title={title}
+        description={description}
+        canonical={canonical}
+        additionalMetaTags={[
+          {
+            property: "keywords",
+            content: "shardeum,blockchain,layer1 blockchain,evm compatible blockchain",
+          },
+          {
+            property: "twitter:image",
+            content: image,
+          },
+        ]}
+        openGraph={{
+          title: title,
+          type: "website",
+          url: canonical,
+
+          description: description,
+          images: [
+            {
+              url: image,
+              alt: "Shardeum Image",
+            },
+          ],
+        }}
+        twitter={{
+          cardType: "summary_large_image",
+          site: "https://shardeum.org",
+          handle: "@shardeum",
+        }}
+      />
+      <Flex bg="brand.white" as="section">
+        <Container
+          maxW="container.xl"
+          mx="auto"
+          py={{ base: "9", md: "10" }}
+          px={{ base: 6, xl: 0 }}
+        >
+          <NotionRenderer
+            recordMap={recordMap}
+            fullPage={false}
+            darkMode={false}
+            components={{ Collection, Tweet }}
+          />
+        </Container>
+      </Flex>
+    </>
+  );
+};
+
+export async function getStaticProps({ locale }: { locale: string }) {
+  const notion = new NotionAPI();
+  const pageId = "proof-of-community-program";
+  // console.log(NotionPagesLinks);
+  let notionPageDetails = { slug: "", notionId: "", title: "", description: "", image: "" };
+  for (const nPage of NotionPagesLinks) {
+    // console.log(nPage);
+    if (nPage.slug == pageId) notionPageDetails = nPage;
+  }
+
+  if (!notionPageDetails.notionId) {
+    //Redirect to 404
+    return {
+      // returns the default 404 page with a status code of 404
+      notFound: true,
+    };
+  }
+  // const notionPageDetails = NotionPagesLinks[pageId];
+  const recordMap = await notion.getPage("b9a51fb534fc4b35ab860164ce4cb15f");
+  // console.log(recordMap);
+  return {
+    props: {
+      recordMap,
+      notionPageDetails,
+      ...(await serverSideTranslations(locale, ["common"])),
+    },
+    revalidate: 10, // In seconds
+  };
+}
+
+export default Page;
